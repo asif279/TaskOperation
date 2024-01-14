b324e65b12827acafbf323c6373f2d90202cd5ac
Asif Mahmud <asifofficial8130@gmail.com>
2024-01-14 15:55:27 +0600

    TaskManagement

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Footer from "./components/Footer/Footer";
+import TaskBoard from "./components/Task/TaskBoard.jsx/TaskBoard";
+
+function App() {
+  return (
+    <section className="bg-[#191D26] font-[Inter] text-white">
+      <Header />
+      <HeroSection />
+      <TaskBoard />
+      <Footer />
+    </section>
+  );
+}
+
+export default App;
